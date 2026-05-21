9d8749f4f2703ae0165df1f599bdd537762ea391
krishpratap2023-blip <krishpratap2023@gmail.com>
2026-05-21 20:02:53 +0530

    Update app.js

---
+++ app.js
@@ -0,0 +1,68 @@
+import { db } from './firebase.js';
+let currentUser=null;
+
+joinBtn.onclick=()=>{
+
+const realName=document.getElementById('realName').value;
+const username=document.getElementById('username').value;
+const relation=document.getElementById('relation').value;
+
+if(!realName||!username){
+alert('Fill all fields');
+return;
+}
+
+currentUser={realName,username,relation};
+
+loginScreen.classList.add('hidden');
+chatApp.classList.remove('hidden');
+};
+
+sendBtn.onclick=sendMessage;
+
+messageInput.addEventListener('keypress',(e)=>{
+if(e.key==='Enter') sendMessage();
+});
+
+async function sendMessage(){
+
+const text=messageInput.value.trim();
+
+if(!text) return;
+
+await addDoc(collection(db,'messages'),{
+text,
+sender:currentUser.realName,
+relation:currentUser.relation,
+createdAt:serverTimestamp()
+});
+
+messageInput.value='';
+}
+
+const q=query(collection(db,'messages'),orderBy('createdAt'));
+
+onSnapshot(q,(snapshot)=>{
+
+messages.innerHTML='';
+
+snapshot.forEach((doc)=>{
+
+const data=doc.data();
+
+const div=document.createElement('div');
+div.className='message';
+
+div.innerHTML=`
+<div>
+<strong>${data.sender}</strong>
+${data.relation==='elder' ? '<span class="tag">🟢 ELDER</span>' : ''}
+</div>
+<div>${data.text}</div>
+`;
+
+messages.appendChild(div);
+});
+
+messages.scrollTop=messages.scrollHeight;
+});
