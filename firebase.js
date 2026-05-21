aa1b4714d64f57c92c4a5aa685c762fa2fe26373
krishpratap2023-blip <krishpratap2023@gmail.com>
2026-05-21 20:16:38 +0530

    Update Firebase initialization and add analytics

--- firebase.js
+++ firebase.js
@@ -1,6 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBV-Rr2H145apqo8HBZhqvs_-qXDLjeEvs",
   authDomain: "timepassers.firebaseapp.com",
@@ -12,5 +17,6 @@ const firebaseConfig = {
   measurementId: "G-DXMJX7Z3W3"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
